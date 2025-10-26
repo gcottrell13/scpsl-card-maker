f439beb2870b9739ce750fc35a219542f726e123
Sage <inquisitorpinkie@gmail.com>
2025-10-26 15:05:40 -0700

    github pages

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,14 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+
+    output: {
+        // Please replace <REPO_NAME> with the repository name.
+        // For example, "/my-project/"
+        assetPrefix: '/scpsl-card-maker/',
+    },
+    html: {
+        title: 'SCPSL Card Maker',
+        favicon: './favicon.ico',
+    },
 });
